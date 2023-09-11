b23891bcbea33f81475d4a31ae8f8e11c9e8edd1
u029299 <miguel.gorostola.ext@rsi.cajarural.com>
2023-09-11 10:09:29 +0200

    Add check password fields

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 const themeSelector = document.getElementById('selector');
 const root = document.documentElement;
 const themeName = document.querySelector('.theme-name');
+const password = document.getElementById('password');
+const errorPassword = document.getElementById('password').nextElementSibling;
+const verifyPassword = document.getElementById('verify-password');
+const errorVerifyPassword = document.getElementById('verify-password').nextElementSibling;
+const formInputs = document.querySelectorAll('.input');
+const submitButton = document.querySelector('.form-button>button');
 
 
 
@@ -20,11 +26,45 @@ function changeThemeName() {
   themeName.textContent = newName;
 }
 
+function checkPasswords() {
+  const comparePasswords = () => {
+    if (password.value !== verifyPassword.value) {
+      errorVerifyPassword.textContent = '*Password do not match';
+      verifyPassword.classList.add('error');
+    } else {
+      if (verifyPassword.classList.contains('error')) {
+        errorVerifyPassword.textContent = '';
+        verifyPassword.classList.remove('error');
+      }
+    }
+  }
+
+  const validatePasswords = () => {
+    if (password.textContent.length < 6) {
+      errorPassword.textContent = '*Password must be at least 6 characters';
+      password.classList.add('error');
+    } else {
+      if (password.classList.contains('error')) {
+        errorPassword.textContent = '';
+        password.classList.remove('error');
+      }
+    }
+  }
+
+  validatePasswords();
+  comparePasswords();
+}
+
 themeSelector.addEventListener('click', () => { 
   setTheme();
   changeThemeName();
 });
 
+submitButton.addEventListener('click', (e) => {
+  checkPasswords();
+})
+
 window.onload = () => {
-  root.className = 'light';
+  root.className = 'dark';
 }
+
